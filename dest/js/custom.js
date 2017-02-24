116da74b5bd6b28c38efd75f44f2a77422741656
kikakikuc <phoebe.ccb@gmail.com>
2017-02-24 14:44:36 +0700

    New responsive version for mobile

--- dest/js/custom.js
+++ dest/js/custom.js
@@ -6,7 +6,6 @@ $('.grid').isotope({
 	itemSelector:'.item',
 	layoutM0de: 'fitRows'
 });
-//Isotope click function
 
 $('.iso-nav ul li').click(function(){
 	$('.iso-nav ul li').removeClass('active');
@@ -15,9 +14,41 @@ $('.iso-nav ul li').click(function(){
 	var selector = $(this).attr('data-filter');
 	$('.grid').isotope({
 		filter:selector,
-		
 	});
 	return false;
 });
 
+$("nav ul li").click(function() {
+	var selectedTab = $(this).attr('tab');
+	$(this).addClass('active').siblings().removeClass('active');
+});
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 825) {
+        $('.menu').addClass('navbar-fixed-top');
+    } else {
+        $('.menu').removeClass('navbar-fixed-top');
+    }
+});
+
+$('.counter').each(function() {
+	var $this = $(this),
+	  countTo = $this.attr('data-count');
+
+	$({ countNum: $this.text()}).animate({
+		countNum: countTo
+		},
+  		{
+	    duration: 20000,
+	    easing:'linear',
+	    step: function() {
+	      $this.text(Math.floor(this.countNum));
+	    },
+	    complete: function() {
+	      $this.text(this.countNum);
+	      //alert('finished');
+	    }
+
+  });
+});
 
