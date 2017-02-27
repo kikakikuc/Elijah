118112494af69a713203fe4112a8f41b446cfe3a
kikakikuc <phoebe.ccb@gmail.com>
2017-02-27 16:14:26 +0700

    Fixed isotope

--- src/js/custom.js
+++ src/js/custom.js
@@ -10,20 +10,26 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
 });
 
-$('.grid').isotope({
-	itemSelector:'.item',
-	layoutM0de: 'fitRows'
+//isotope
+
+$(".grid").isotope({
+	itemSelector: '.grid-item',
+	layoutMode: 'fitRows'
 });
-
 $('.iso-nav ul li').click(function(){
-	$('.iso-nav ul li').removeClass('active');
-	$(this).addClass('active');
-
-	var selector = $(this).attr('data-filter');
-	$('.grid').isotope({
-		filter:selector,
-	});
-	return false;
+    $('.iso-nav ul li').removeClass('active');
+    $(this).addClass('active');
+    
+    var selector =$(this).attr('data-filter');
+    $(".grid").isotope({
+        filter: selector,
+        animationOption: {
+	         duration: 750,
+	         easing: 'linear',
+	         queue: false,
+        }
+    });
+    return false;
 });
 
 $("nav ul li").click(function() {
